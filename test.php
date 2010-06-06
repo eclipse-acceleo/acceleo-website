4b59440f50aeaaa24e87e0a7a12de522bacb1764
lgoubet <lgoubet>
2010-06-06 14:22:21 +0000

    commit draft of an rss2html utility

--- test.php
+++ test.php
@@ -1,11 +1,14 @@
 <?php  																														
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
+require_once("script/rss2html.php""); 	
 $App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		
-include($App->getProjectCommon());
+include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	
-	$html = file_get_contents('_index.html');
+	
+	# Paste your HTML content between the EOHTML markers!
+	$html = rss2html();
 
 	# Generate the web page
 	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
