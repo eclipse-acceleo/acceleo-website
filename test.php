006a20331080130c625e00ebdbccd69f80520a46
lgoubet <lgoubet>
2010-06-06 14:34:14 +0000

    debugging php script

--- test.php
+++ test.php
@@ -2,15 +2,12 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
-require_once("script/rss2html.php"); 	
+	
 $App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		
 include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	
-	
-	# Paste your HTML content between the EOHTML markers!
-	$html = rss2html();
+	$html = "debug";
 
 	# Generate the web page
 	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
-	#echo $headlines;
 ?>
