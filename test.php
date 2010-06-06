468d97ca4ffed0e26442f714865c76c56dbe8d32
lgoubet <lgoubet>
2010-06-06 14:42:19 +0000

    debugging php script

--- test.php
+++ test.php
@@ -6,6 +6,7 @@ require_once("script/rss2html.php");
 $App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		
 include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	
+	$newsConverter = new RSS2HTML();
 	$html = "debug1";
 
 	# Generate the web page
