3a8b189b5dab00d394e4e007fa308fc2a5ef055a
lgoubet <lgoubet>
2010-06-06 14:30:48 +0000

    debugging php script

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
-require_once("script/rss2html.php""); 	
+require_once("script/rss2html.php"); 	
 $App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		
 include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	
