3e1c86007358ca51628c75fb12c14975b2799324
lgoubet <lgoubet>
2010-06-06 14:42:18 +0000

    debugging php script

--- script/rss2html.php
+++ script/rss2html.php
@@ -5,103 +5,104 @@ $limitTitleLength = 15;
 $limitDescriptionLength = 30;
 $feedURL = "http://www.acceleo.org/planet/rss20.xml";
 
-// Global variables used throughout the script
-$readError = "";
+class RSS2HTML {
+	$readError = "";
 
-Function rss2html() {
-	$result = "";
-	$xmlString = readFeed();
-	if ($xmlString == NULL) {
-		$result = $readError;
-		return $result;
-	}
-
-	$xmlParser = xml_parser_create();
-	$rssParser = new RSSParser();
-	xml_set_object($xmlParser, $rssParser);
-	xml_set_element_handler($xmlParser, "startElement", "endElement");
-	xml_set_character_data_handler($xmlParser, "characterData");
-	$parseResult = xml_parse($xmlParser, $xmlString, TRUE);
-	if ($parseResult == 0) {
-		$result = xml_error_string(xml_get_error_code($xmlParser));
-		$result .= "\nat ".xml_get_current_line_number($xmlParser);
-		$result .= ":".xml_get_current_column_number($xmlParser);
-		return $result;
-	}
-	
-	$itemCount = min($limitItem, count($rssParser->items));
-	
-	if ($itemCount > 0) {
-		$result = "<h3><a href=\"".$rssParser->feed->link."\">".$rssParser->feed->title."</a></h3>\n";
-		$result .= "<ul>\n";
-		for ($i = 0; $i < $itemCount; $i++) {
-			$item = $rssParser->items[$i];
-			$result .= "<li>";
-			$result .= "<div id=\"meta\">".$item->pubDate_time."</div>";
-			$result .= "<a href=\"".$item->link."\" display=\"block\">".limitLength($item->title, $limitTitleLength)."</a>";
-			$result .= limitLength($item->description, $limitDescriptionLength);
-			$result .= "</li>\n";
+	function rss2html() {
+		$result = "";
+		$xmlString = readFeed();
+		if ($xmlString == NULL) {
+			$result = $readError;
+			return $result;
 		}
-		$result .= "</ul>\n";
+
+		$xmlParser = xml_parser_create();
+		$rssParser = new RSSParser();
+		xml_set_object($xmlParser, $rssParser);
+		xml_set_element_handler($xmlParser, "startElement", "endElement");
+		xml_set_character_data_handler($xmlParser, "characterData");
+		$parseResult = xml_parse($xmlParser, $xmlString, TRUE);
+		if ($parseResult == 0) {
+			$result = xml_error_string(xml_get_error_code($xmlParser));
+			$result .= "\nat ".xml_get_current_line_number($xmlParser);
+			$result .= ":".xml_get_current_column_number($xmlParser);
+			return $result;
+		}
+		
+		$itemCount = min($limitItem, count($rssParser->items));
+		
+		if ($itemCount > 0) {
+			$result = "<h3><a href=\"".$rssParser->feed->link."\">".$rssParser->feed->title."</a></h3>\n";
+			$result .= "<ul>\n";
+			for ($i = 0; $i < $itemCount; $i++) {
+				$item = $rssParser->items[$i];
+				$result .= "<li>";
+				$result .= "<div id=\"meta\">".$item->pubDate_time."</div>";
+				$result .= "<a href=\"".$item->link."\" display=\"block\">".limitLength($item->title, $limitTitleLength)."</a>";
+				$result .= limitLength($item->description, $limitDescriptionLength);
+				$result .= "</li>\n";
+			}
+			$result .= "</ul>\n";
+		}
+		
+		// debug
+		$result = "<h3>debug</h3>";
+		$result .= $xmlString;
+		return $result;
 	}
-	
-	// debug
-	$result = "<h3>debug</h3>";
-	$result .= $xmlString;
-	return $result;
-}
 
-/*
- * Reads the feed denoted by URL $feedURL in memory. Any error will be logged within $readError.
- */
-Function readFeed() {
-	$result = "";
+	/*
+	 * Reads the feed denoted by URL $feedURL in memory. Any error will be logged within $readError.
+	 */
+	function readFeed() {
+		$result = "";
 
-	$curlHandle = curl_init();
-	curl_setopt($curlHandle, CURLOPT_URL, $feedURL);
-	curl_setopt($curlHandle, CURLOPT_HEADER, 0);
-	curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curlHandle, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.1;) Gecko/2008070208 Firefox/3.0.1");
-	curl_setopt($curlHandle, CURLOPT_REFERER, $feedURL);
-	curl_setopt($curlHandle, CURLOPT_CONNECTTIMEOUT, 5000);
-	curl_setopt($curlHandle, CURLOPT_MAXREDIRS, 10);
-	curl_setopt($curlHandle, CURLOPT_ENCODING, 'identity');
-	
-	$result = curl_exec($curlHandle);
-	if (curl_errno($curlHandle)) {
-		$readError = curl_error($curlHandle);
+		$curlHandle = curl_init();
+		curl_setopt($curlHandle, CURLOPT_URL, $feedURL);
+		curl_setopt($curlHandle, CURLOPT_HEADER, 0);
+		curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curlHandle, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.1;) Gecko/2008070208 Firefox/3.0.1");
+		curl_setopt($curlHandle, CURLOPT_REFERER, $feedURL);
+		curl_setopt($curlHandle, CURLOPT_CONNECTTIMEOUT, 5000);
+		curl_setopt($curlHandle, CURLOPT_MAXREDIRS, 10);
+		curl_setopt($curlHandle, CURLOPT_ENCODING, 'identity');
+		
+		$result = curl_exec($curlHandle);
+		if (curl_errno($curlHandle)) {
+			$readError = curl_error($curlHandle);
+			curl_close($curlHandle);
+			return NULL;
+		}
+		
+		$httpResponse = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
+		if ($httpResponse < 200 || $httpResponse >= 300) {
+			$readError = "HTTP ERROR: $httpResponse";
+			curl_close($curlHandle);
+			return NULL;
+		}
+		
 		curl_close($curlHandle);
-		return NULL;
-	}
-	
-	$httpResponse = curl_getinfo($curlHandle, CURLINFO_HTTP_CODE);
-	if ($httpResponse < 200 || $httpResponse >= 300) {
-		$readError = "HTTP ERROR: $httpResponse";
-		curl_close($curlHandle);
-		return NULL;
-	}
-	
-	curl_close($curlHandle);
-	return $result;
-}
-
-/*
- * Limit the length of the given String to the given number of characters.
- */
-Function limitLength($initialValue, $limit = -1) {
-	if ($limit == -1 || strlen($initialValue) <= $limit) {
-		return $initialValue;
+		return $result;
 	}
 
-	$result = substr($initialValue, 0, $limit);
+	/*
+	 * Limit the length of the given String to the given number of characters.
+	 */
+	function limitLength($initialValue, $limit = -1) {
+		if ($limit == -1 || strlen($initialValue) <= $limit) {
+			return $initialValue;
+		}
 
-	$lastSpace = strrchr($result, ' ');
-	if ($lastSpace != FALSE) {
-		$result = substr($result, 0, -strlen($lastSpace));
-		$result .= " [...]";
+		$result = substr($initialValue, 0, $limit);
+
+		$lastSpace = strrchr($result, ' ');
+		if ($lastSpace != FALSE) {
+			$result = substr($result, 0, -strlen($lastSpace));
+			$result .= " [...]";
+		}
+
+		return $result;
 	}
-
-	return $result;
 }
 
 class RSSParser {
--- test.php
+++ test.php
@@ -2,11 +2,11 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
-	
+require_once("script/rss2html.php");
 $App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		
 include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	
-	$html = "debug";
+	$html = "debug1";
 
 	# Generate the web page
 	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
