631febce07f94f02b5f5750b74e13c4efbcc573f
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2014-06-25 10:37:19 +0200

    Add back some necessary PHP variables and dependencies.

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
+<?php  
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
+
+	$App 	= new App();
+	$Nav	= new Nav();
+	$Menu 	= new Menu();
+
+	include($App->getProjectCommon());
+
 	$pageKeywords	= "acceleo, dsl, modeling, domain specific language, textual, generator, code generator, emf, mofm2t, dsl, metamodel, free, open source, uml";
 	$pageAuthor		= "Obeo";
 	$pageTitle 		= "Acceleo";
