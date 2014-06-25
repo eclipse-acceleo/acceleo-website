1a81601ef4e5bf28596ed05592c4ebdd1f965552
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2014-06-25 11:35:35 +0200

    Retrieve the prezi presentation using HTTPS.

--- downloads/index.php
+++ downloads/index.php
@@ -52,5 +52,5 @@
 	$html .= "</div>\n\n";
 	
 	# Generate the web page
-	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
